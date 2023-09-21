1ef55280053d7651948d80bb81748b38b87f0b44
hannah1590 <hannah.fasco@mymail.champlain.edu>
2023-09-21 12:51:05 -0400

    worked on rng

--- courses/artificialintelligence/assignments/rng/rng.cpp
+++ courses/artificialintelligence/assignments/rng/rng.cpp
@@ -1,4 +1,27 @@
-// add your imports here
+#include <iostream>
+using namespace std;
+
 int main(){
-  // code here
+  unsigned int seed;
+  unsigned int amount;
+  signed int min;
+  signed int max;
+
+  unsigned int xorShift;
+  int currentRandom;
+  cin >> seed >> amount >> min >> max;
+
+  for(int i = 0; i < amount; i++) {
+      xorShift = seed << 13; // shift seed 13 bits to the left
+      seed ^= xorShift; // xor of the original seed and the shifted seed
+      xorShift = seed >> 17;
+      seed ^= xorShift;
+      xorShift = seed << 5;
+      seed ^= xorShift;
+
+      currentRandom = seed;
+      currentRandom = min + (seed % (max - min + 1));
+
+      cout << currentRandom << "\n";
+  }
 }
