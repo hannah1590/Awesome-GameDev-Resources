d5ebeea9c9fca141de889139cf337dd705774f63
hannah1590 <hannah.fasco@mymail.champlain.edu>
2023-10-08 17:25:20 -0400

    base cat code

--- courses/artificialintelligence/assignments/catchthecat/Cat.h
+++ courses/artificialintelligence/assignments/catchthecat/Cat.h
@@ -3,8 +3,60 @@
 #include "IAgent.h"
 
 struct Cat : public IAgent {
+
   std::pair<int,int> move(const std::vector<bool>& world, std::pair<int,int> catPos, int sideSize ) override{
-    return {0,0}; // todo: change this
+     std::vector<std::pair<int,int>> neighbors;
+     int halfSideSize = sideSize / 2;
+
+     if(catPos.first + 1 <= halfSideSize && !world[(catPos.second + halfSideSize) * sideSize + (catPos.first + 1 + halfSideSize)])
+     {
+         neighbors.emplace_back(catPos.first + 1,catPos.second);
+     }
+     if(catPos.first - 1 >= -halfSideSize && !world[(catPos.second + halfSideSize) * sideSize + (catPos.first - 1 + halfSideSize)])
+     {
+         neighbors.emplace_back(catPos.first - 1,catPos.second);
+     }
+
+     // upright
+     if(catPos.second - 1 >= -halfSideSize && catPos.second % 2 == 0 && !world[(catPos.second - 1 + halfSideSize) * sideSize + (catPos.first + halfSideSize)])
+     {
+         neighbors.emplace_back(catPos.first,catPos.second - 1);
+     }
+     if(catPos.first + 1 <= halfSideSize && catPos.second - 1 >= -halfSideSize && catPos.second % 2 == 1 && !world[(catPos.second - 1 + halfSideSize) * sideSize + (catPos.first + 1 + halfSideSize)])
+     {
+         neighbors.emplace_back(catPos.first + 1,catPos.second - 1);
+     }
+
+     // upleft
+     if(catPos.first - 1 >= -halfSideSize && catPos.second - 1 >= -halfSideSize && catPos.second % 2 == 0 && !world[(catPos.second - 1 + halfSideSize) * sideSize + (catPos.first - 1 + halfSideSize)])
+     {
+         neighbors.emplace_back(catPos.first - 1,catPos.second - 1);
+     }
+     if(catPos.second - 1 >= -halfSideSize && catPos.second % 2 == 1 && !world[(catPos.second - 1 + halfSideSize) * sideSize + (catPos.first + halfSideSize)])
+     {
+         neighbors.emplace_back(catPos.first,catPos.second - 1);
+     }
+
+     // downright
+     if(catPos.second + 1 <= halfSideSize && catPos.second % 2 == 0 && !world[(catPos.second + 1 + halfSideSize) * sideSize + (catPos.first + halfSideSize)])
+     {
+         neighbors.emplace_back(catPos.first,catPos.second + 1);
+     }
+     if(catPos.first + 1 <= halfSideSize && catPos.second + 1 <= halfSideSize && catPos.second % 2 == 1 && !world[(catPos.second + 1 + halfSideSize) * sideSize + (catPos.first + 1 + halfSideSize)])
+     {
+         neighbors.emplace_back(catPos.first + 1,catPos.second + 1);
+     }
+
+     // downleft
+     if(catPos.first - 1 >= -halfSideSize && catPos.second + 1 <= halfSideSize && catPos.second % 2 == 0 && !world[(catPos.second + 1 + halfSideSize) * sideSize + (catPos.first - 1 + halfSideSize)])
+     {
+         neighbors.emplace_back(catPos.first - 1,catPos.second + 1);
+     }
+      if(catPos.second + 1 <= halfSideSize && catPos.second % 2 == 1 && !world[(catPos.second + 1 + halfSideSize) * sideSize + (catPos.first + halfSideSize)])
+      {
+          neighbors.emplace_back(catPos.first,catPos.second + 1);
+      }
+      return neighbors.back(); // todo: change this
   }
 };
 #endif
